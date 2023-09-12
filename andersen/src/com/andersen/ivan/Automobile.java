4c8fb048ff66ad924e459398c5cfef6af1c7f3e9
Ivan <ivantumentsev@gmail.com>
2023-09-12 15:33:21 +0300

    commit in andersen with 2 classes

---
+++ andersen/src/com/andersen/ivan/Automobile.java
@@ -0,0 +1,27 @@
+package com.andersen.ivan;
+
+public class Automobile {
+    private int year;
+    private String name;
+
+    public Automobile(int year, String name){
+        this.year = year;
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
